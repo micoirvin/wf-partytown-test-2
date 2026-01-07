c0b2521ea1c36e9e89ff7955ff4053ff3200f10c
Mico Irvin De Mesa <104616752+micoirvin@users.noreply.github.com>
2026-01-07 21:46:26 +0800

    Update build to include all partytown

--- bin/build.js
+++ bin/build.js
@@ -7,7 +7,13 @@ const BUILD_DIRECTORY = 'dist';
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
 // Config entrypoint files
-const ENTRY_POINTS = ['src/index.ts', 'src/~partytown/partytown.js'];
+const ENTRY_POINTS = [
+  'src/index.ts',
+  'src/~partytown/partytown.js',
+  'src/~partytown/partytown-sw.js',
+  'src/~partytown/partytown-media.js',
+  'src/~partytown/partytown-atomics.js',
+];
 
 // Config dev serving
 const LIVE_RELOAD = !PRODUCTION;
